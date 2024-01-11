e809b68511fcff3b9d8ea8b9a96fa573e9774b6a
Nicolas Thompson <mail@nicolasthompson.com>
2024-01-11 11:24:02 +0000

    first commit

---
+++ src/Server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+const port = 3000;
+export class Server {
+  private app = express();
+
+  startServer() {
+    this.app.get('/hello', (req: Request, res: Response) => {
+      res.send('Hello');
+    });
+    this.app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  }
+}
+
+new Server().startServer();
